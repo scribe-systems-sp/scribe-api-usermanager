b0ef81d935c0c17f0142fba7180af7f25f98bfb7
Mykola Zhyhallo <mzhyhall@gmail.com>
2020-08-18 20:20:57 +0200

    Fix import to use modularus from new source

--- modularus.ts
+++ modularus.ts
@@ -1,4 +1,4 @@
-import { SApi, Modularus } from 'modularus'
+import { SApi, Modularus } from '@scribe-systems/modularus'
 import Axios, { AxiosStatic } from 'axios'
 
 import { APIKeysApi, AccessRightsApi, AuthApi, UsersApi } from './api'
